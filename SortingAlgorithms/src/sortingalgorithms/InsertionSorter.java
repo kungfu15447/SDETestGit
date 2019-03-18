70cf182a2fc75f3f258687647778bae9a2e3e69b
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-03-18 12:45:17 +0100

    asd

--- SortingAlgorithms/src/sortingalgorithms/InsertionSorter.java
+++ SortingAlgorithms/src/sortingalgorithms/InsertionSorter.java
@@ -19,11 +19,13 @@ public class InsertionSorter
         {
             int key = a[i];
             int j = i - 1;
+            
             while (j >= 0 && a[j] > key)
             {
                 a[j + 1] = a[j];
                 j = j - 1;
             }
+            
             a[j + 1] = key;
         }
     }
